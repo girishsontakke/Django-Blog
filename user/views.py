a2e09b782b9ddbb1a6b6967312ca5e6b6b46ab90
girishsontakke <girishsontakke7@gmail.com>
2020-09-11 18:35:10 +0530

    logic for user validation and creation

--- user/views.py
+++ user/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from user.forms import UserRegisterForm
 
 # Create your views here.
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Account Created for {username}!")
+            return redirect("blog:blog-home")
+
+    else:
+        form = UserRegisterForm()
+    return render(request, "user/register.html", {'form': form})
