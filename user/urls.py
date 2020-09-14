345a47e2b92c09c93b2ff3c2c20da18868c30244
girishsontakke <girishsontakke7@gmail.com>
2020-09-14 14:08:51 +0530

    add url for profile and api

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
-from user.views import register
+from django.urls import path, include
+from user import views as user_view
 from django.contrib.auth import views as auth_views
 app_name = "user"
 urlpatterns = [
-    path("register/", register, name="register"),
+    path("register/", user_view.register, name="register"),
+    path("profile/", user_view.profile, name="profile"),
     path("login/", auth_views.LoginView.as_view(template_name="user/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name="user/logout.html"), name="logout"),
+    path("api/", include("user.api.urls"))
 ]
