fdd5eece23d22da369083efaed0427495a8caacd
girishsontakke <girishsontakke7@gmail.com>
2020-09-14 18:15:44 +0530

    add url for rest_framework login

--- blog_application/urls.py
+++ blog_application/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')
          ),
-    path("user/", include("user.urls"))
+    path("user/", include("user.urls")),
+    path("api-auth/", include("rest_framework.urls"))
 ]
