907d64074f26190b306b79dc39454463d0448118
girishsontakke <girishsontakke7@gmail.com>
2020-09-17 12:09:55 +0530

    validate and save the info of userupdateform and profileupdateform

--- user/views.py
+++ user/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from user.forms import UserRegisterForm
+from user.forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 # Create your views here.
 
@@ -23,4 +23,21 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, "user/profile.html")
+    if request.method == "GET":
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    else:
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f"Your Profile has been updated 🚀")
+            return redirect("user:profile")
+
+    context = {
+        "u_form": u_form,
+        "p_form": p_form
+    }
+    return render(request, "user/profile.html", context)
