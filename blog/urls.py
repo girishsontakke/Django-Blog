17cf74ab68dcd7149fef9f387ac1f97ea57fc3a2
girishsontakke <girishsontakke7@gmail.com>
2020-09-22 11:26:01 +0530

    post detail page

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.PostListView.as_view(), name='home'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='about')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views import generic
 
 
 def home(request):
@@ -8,6 +9,17 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(generic.ListView):
+    model = Post
+    queryset = Post.objects.all().order_by("-date_posted")
+    template_name = "blog/home.html"
+    context_object_name = "posts"
+
+
+class PostDetailView(generic.DetailView):
+    model = Post
 
 
 def about(request):
