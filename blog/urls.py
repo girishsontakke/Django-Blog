9e0acab28f6994c9f6454645709ca91bacf4a321
girishsontakke <girishsontakke7@gmail.com>
2020-10-09 11:10:57 +0530

    fix routing

--- blog/urls.py
+++ blog/urls.py
@@ -4,10 +4,11 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.PostListView.as_view(), name='home'),
-    path('user/<str:username>/', views.UserPostListView.as_view(), name='user-post'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('post/new/', views.PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('post/<str:username>/',
+         views.UserPostListView.as_view(), name='user-post'),
 ]
